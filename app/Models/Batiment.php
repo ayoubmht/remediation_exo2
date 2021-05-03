ac8493af1c0619e7cc16ae398bda69a3e75b21c5
Ayoub Mohat <ayoubmht@MacBook-Pro-de-Ayoub.local>
2021-05-03 17:09:03 +0200

    debut

---
+++ app/Models/Batiment.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Batiment extends Model
+{
+    use HasFactory;
+
+    protected $table = "batiments";
+
+    protected $filable = [
+        "nom",
+        "adresse",
+        "description",
+    ];
+}
---
+++ resources/views/batiment.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel=“stylesheet” href={{ asset("css/app.css") }}>
+</head>
+
+<body>
+    <section class="container">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nom</th>
+                    <th scope="col">Adresse</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Actions</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($batiments as $batiment)
+                    <tr>
+                        <th scope="row">{{ $batiment->id }}</th>
+                        <td>{{ $batiment->nom }}</td>
+                        <td>{{ $batiment->adresse }}</td>
+                        <td>{{ $batiment->description }}</td>
+                        <td>
+                            <form action="">
+                                <button class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </section>
+
+
+
+
+
+    <script src={{ asset('js/app.js') }}></script>
+</body>
+
+</html>
